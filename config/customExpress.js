aded11c8fea822ab82f2e0724abbd89a3c03bc2e
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2021-04-17 18:03:44 -0300

    autorização do cors

--- config/customExpress.js
+++ config/customExpress.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 module.exports = () => {
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     const app = express();
     app.use(cors());
     app.use(bodyParser.urlencoded({ extended: true }));
