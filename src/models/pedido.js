2e2961940d5f937774a46d7efe52340688b0b489
Lucas Diniz <diniz3003@gmail.com>
2021-04-25 17:32:35 -0300

    latitude e longitude

--- src/models/pedido.js
+++ src/models/pedido.js
@@ -3,6 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 class Pedido extends Model {
     static init(sequelize){
         super.init({
+            latitude: DataTypes.NUMBER,
+            longitude: DataTypes.NUMBER,
+            status: DataTypes.STRING,
+            total: DataTypes.NUMBER
         }, {sequelize})
     }
 }
