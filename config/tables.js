7f54102702ce3f0c9a73775658e3a09625b68841
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2021-04-27 13:32:46 -0300

    atualização das tabelas

--- config/tables.js
+++ config/tables.js
@@ -5,13 +5,15 @@ class Tables {
     }
     createProdutos() {
 
-        const sql = "CREATE TABLE IF NOT EXISTS Produtos (id INT NOT NULL AUTO_INCREMENT, TITULO VARCHAR(100) NOT NULL, PRECO FLOAT NOT NULL, DESCRICAO TEXT, ATIVO BOOLEAN, IMAGEM VARCHAR(50), PRIMARY KEY(id))";
+        const sql = `CREATE TABLE IF NOT EXISTS Produtos (id INT NOT NULL AUTO_INCREMENT, TITULO VARCHAR(100) NOT NULL, PRECO FLOAT NOT NULL, DESCRICAO TEXT, ATIVO BOOLEAN, IMAGEM VARCHAR(50), PRIMARY KEY(id))
+                     CREATE TABLE IF NOT EXISTS Usuarios (id INT NOT NULL AUTO_INCREMENT, NOME VARCHAR(100) NOT NULL, IDADE INT, GENERO VARCHAR(1), FOTO VARCHAR(50), PRIMARY KEY(id))             
+                     CREATE TABLE IF NOT EXISTS Exercicios (id INT NOT NULL AUTO_INCREMENT, TITULO VARCHAR(100) NOT NULL, REPETICAO VARCHAR(100) NOT NULL, DESCRICAO TEXT, IMAGEM VARCHAR(50), PESSOA_ID INT NOT NULL, PRIMARY KEY(id))`;
 
         this.conn.query(sql, (err) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log("Tabela Produtos OK.");
+                console.log("Tabelas criadas.");
             }
         });
     }
