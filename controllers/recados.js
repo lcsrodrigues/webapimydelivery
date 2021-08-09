1ea6c72a3905baae1f82162925ae542200bd94e7
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2021-08-08 23:24:36 -0300

    Correção no caminho da pasta uploads

--- controllers/recados.js
+++ controllers/recados.js
@@ -3,10 +3,10 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/uploads/');
+        cb(null, './uploads/');
     },
     filename: function (req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
+        cb(null, new Date().toISOString() + "_" + file.originalname);
     }
 });
 
